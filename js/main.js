703fbf48efdbd43dde9a49b2431074654e30e6d6
Sasha Pylypenko <Sashapylypenko.pov@gmail.com>
2023-05-08 15:52:27 +0300

    feat:L init carousel

--- js/main.js
+++ js/main.js
@@ -5,3 +5,33 @@ menuToggle.addEventListener("click", () => {
   menuToggle.classList.toggle("active");
   menu.classList.toggle("active");
 });
+
+const sliderTrack = document.querySelector(".slider__track");
+const sliderItems = document.querySelectorAll(".slider__track .slider__item");
+
+console.log(sliderTrack, "sliderTrack");
+console.log(sliderItems, "sliderItems");
+
+function prepareSlider(direction, speed = 10) {
+  let height = 0;
+  let width = 0;
+  let gap = 36;
+  sliderItems.forEach((item) => {
+    height = height + item.offsetHeight + gap;
+  });
+
+  console.log(height, "height");
+
+  sliderTrack.animate(
+    [
+      { transform: " transform: translateY(000px);" },
+      { transform: `translateY(-${860}px)` },
+    ],
+    {
+      duration: 4000,
+      iterations: Infinity,
+    }
+  );
+}
+
+prepareSlider();
